96b118cdd8422b1650b568ef8fd081e3a2fa2d6e
Rajat sharma <143316808+RajatSharma070904@users.noreply.github.com>
2024-11-17 19:42:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path  = require("path");
+
+const app = express();
+const PORT = 3000;
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.get("/hobbies",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'hobbies.html'));
+})
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'about.html'));
+})
+app.get("/project",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'project.html'));
+})
+app.get("/interest",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'interest.html'));
+})
+app.listen(PORT,()=>{
+    console.log("server started on: ",`http://localhost:${PORT}`);
+})
---
+++ public/index.js
@@ -0,0 +1,35 @@
+const open_about=()=>{
+    let about_pop_up = document.getElementById("about_pop_up");
+    about_pop_up.style.visibility = "visible";
+}
+const close_about=()=>{
+    let about_pop_up = document.getElementById("about_pop_up");
+    about_pop_up.style.visibility = "hidden";
+}
+
+const open_Services=()=>{
+    let about_pop_up = document.getElementById("Services_pop_up");
+    about_pop_up.style.visibility = "visible";
+}
+const close_Services=()=>{
+    let about_pop_up = document.getElementById("Services_pop_up");
+    about_pop_up.style.visibility = "hidden";
+}
+
+const open_Project=()=>{
+    let about_pop_up = document.getElementById("Project_pop_up");
+    about_pop_up.style.visibility = "visible";
+}
+const close_Project=()=>{
+    let about_pop_up = document.getElementById("Project_pop_up");
+    about_pop_up.style.visibility = "hidden";
+}
+
+const open_Contact_Me=()=>{
+    let about_pop_up = document.getElementById("Contact_Me_pop_up");
+    about_pop_up.style.visibility = "visible";
+}
+const close_Contact_Me=()=>{
+    let about_pop_up = document.getElementById("Contact_Me_pop_up");
+    about_pop_up.style.visibility = "hidden";
+}
